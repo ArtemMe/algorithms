835e044452db36f82988d59b2508c730c790aa96
Артем Межеловский <amejelovskiy@mmtr.ru>
2019-06-04 11:17:02 +0300

    add tests

---
+++ src/java/selection_sort/TestSelectionSort.java
@@ -0,0 +1,19 @@
+package selection_sort;
+
+import org.junit.Test;
+
+public class TestSelectionSort {
+    private int[] arr = {3,92,5,7,7,0};
+
+    @Test
+    public void test1(){
+        int[] res = SelectionSort.sort(arr);
+        for(int i : res)System.out.println(i);
+    }
+
+    @Test
+    public void test2(){
+        int[] res = SelectionSort.sort2(arr);
+        for(int i : res)System.out.println(i);
+    }
+}
