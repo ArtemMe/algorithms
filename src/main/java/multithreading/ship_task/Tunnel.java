688b8cae1a4a20fba67c153ff2a50eba1bae16ba
Артем Межеловский <amejelovskiy@mmtr.ru>
2019-11-03 16:05:08 +0300

    add class tunnel

---
+++ src/main/java/multithreading/ship_task/Tunnel.java
@@ -0,0 +1,63 @@
+package multithreading.ship_task;
+
+import multithreading.ship_task.ships.Ship;
+import multithreading.ship_task.ships.types.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс тунель, в него можно добавлять и извлекать корабли. Максимальная вместиомсть 5 кораблей.
+ * */
+
+public class Tunnel {
+    private List<Ship> store;
+    private static final int maxShipsInTunel = 5;
+    private static final int minShipsInTunel = 0;
+    private int shipsCounter = 0;
+
+    public Tunnel() {
+        store = new ArrayList<>();
+    }
+
+    public synchronized boolean add(Ship element) {
+        try {
+            if(shipsCounter < maxShipsInTunel) {
+                notifyAll();
+                store.add(element);
+                System.out.println(String.format("%s + The ship arrived in the tunnel: %s %s %s",
+                        store.size(), element.getType(), element.getSize(), Thread.currentThread().getName()));
+                shipsCounter++;
+            }else {
+                System.out.println(store.size() + "> There is no place for a ship in the tunnel: " + Thread.currentThread().getName());
+                wait();
+                return false;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public synchronized Ship get(Type shipType) {
+        try {
+            if(shipsCounter > minShipsInTunel) {
+                notifyAll();
+                for(Ship s : store) {
+                    if(s.getType() == shipType) {
+                        shipsCounter --;
+                        System.out.println(store.size() + "- The ship is taken from the tunnel: " + Thread.currentThread().getName());
+                        store.remove(s);
+                        return s;
+                    }
+                }
+            }
+            System.out.println("0 < There are no ships in the tunnel");
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
