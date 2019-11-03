35af8aa56032b82e4700fb48a2c08ce945c6ddc5
Mezhelovsky Artem <lbattersu@gmail.com>
2019-11-03 09:56:38 +0300

    searching multithreading

--- src/main/java/multithreading/searching_files/with_multi/Main.java
+++ src/main/java/multithreading/searching_files/with_multi/Main.java
@@ -1,18 +1,26 @@
 package multithreading.searching_files.with_multi;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         BacklogThreadPool pool = new BacklogThreadPool(4, null);
         String pattern = ".*\\.mp3$";
-        File dir = new File("C:\\Users\\amejelovskiy\\Music\\");
+        File dir = new File("D:\\torrent");
         List list = new ArrayList();
         list.add(dir);
-
+        Long before =  System.nanoTime();
         ParseHandler parseHandler = new ParseHandlerImpl(pattern, pool);
         parseHandler.schedule(TaskFactory.createWalking1Task(parseHandler, list));
+
+        Long after = System.nanoTime();
+
+        System.out.println((after - before)/1000000000+" с");
     }
 }
---
+++ src/main/java/multithreading/ship_task/Tunnel.java
@@ -0,0 +1,65 @@
+package multithreading.ship_task;
+
+import multithreading.ship_task.ships.Ship;
+import multithreading.ship_task.ships.types.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tunnel {
+
+    private List<Ship> store;
+    private static final int maxShipsInTunel = 5;
+    private static final int minShipsInTunel = 0;
+    private int shipsCounter = 0;
+
+    public Tunnel() {
+        store = new ArrayList<>();
+    }
+
+    public synchronized boolean add(Ship element) {
+
+        try {
+            if (shipsCounter < maxShipsInTunel) {
+                notifyAll();
+                store.add(element);
+                String info = String.format("%s + The ship arrived in the tunnel: %s %s %s", store.size(), element.getType(), element.getSize(), Thread.currentThread().getName());
+                System.out.println(info);
+                shipsCounter++;
+
+            } else {
+                System.out.println(store.size() + "> There is no place for a ship in the tunnel: " + Thread.currentThread().getName());
+                wait();
+                return false;
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public synchronized Ship get(Type shipType) {
+
+        try {
+            if (shipsCounter > minShipsInTunel) {
+                notifyAll();
+                for (Ship ship : store) {
+                    if (ship.getType() == shipType) {
+                        shipsCounter--;
+                        System.out.println(store.size() + "- The ship is taken from the tunnel: " + Thread.currentThread().getName());
+                        store.remove(ship);
+                        return ship;
+                    }
+                }
+            }
+
+            System.out.println("0 < There are no ships in the tunnel");
+            wait();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
